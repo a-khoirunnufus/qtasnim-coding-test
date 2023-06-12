47de782d11d86afc84d24a05135048da2506af83
Ahmad Khoirunnufus <a.khoirunnufus@gmail.com>
2023-06-12 10:05:41 +0700

    Update database_dump.sql

--- database_dump.sql
+++ database_dump.sql
@@ -18,24 +18,19 @@ create table public.product (
 	deleted_at timestamp null
 );
 
-alter table public.product add constraint product_category_id_fk
-foreign key (category_id) references public.product_category (id)
+alter table public.product add constraint product_category_id_fk 
+foreign key (category_id) references public.product_category (id) 
 on delete restrict on update cascade;
 
 create table public.transaction (
 	id serial primary key,
-	product_id integer not null,
+	product_name varchar(50) not null,
+	category_name varchar(50) not null,
 	quantity integer not null,
-	created_at timestamp null,
-	updated_at timestamp null,
-	deleted_at timestamp null
+	transaction_date timestamp not null
 );
 
-alter table public.transaction add constraint transaction_product_id
-foreign key (product_id) references public.product (id)
-on delete restrict on update cascade;
-
-insert into public.product_category (id, category_name, created_at, updated_at)
+insert into public.product_category (id, category_name, created_at, updated_at) 
 values
 	(1, 'Konsumsi', now(), now()),
 	(2, 'Pembersih', now(), now());
@@ -52,30 +47,30 @@ values
 
 select setval('product_id_seq', 5);
 
-insert into public.transaction (product_id, quantity, created_at)
-values (1, 10, '2021-05-01'::timestamp);
-update public.product set available = 100 - 10 where id = 1;
+insert into public.transaction (product_name, category_name, quantity, transaction_date)
+values ('Kopi', 'Konsumsi', 10, '2021-05-01'::timestamp);
+update public.product set available = 100 - 10, sold = 10 where id = 1;
 
-insert into public.transaction (product_id, quantity, created_at)
-values (2, 19, '2021-05-05'::timestamp);
-update public.product set available = 100 - 19 where id = 2;
+insert into public.transaction (product_name, category_name, quantity, transaction_date) 
+values ('Teh', 'Konsumsi', 19, '2021-05-05'::timestamp);
+update public.product set available = 100 - 19, sold = 19 where id = 2;
 
-insert into public.transaction (product_id, quantity, created_at)
-values (1, 15, '2021-05-10'::timestamp);
-update public.product set available = 90 - 15 where id = 1;
+insert into public.transaction (product_name, category_name, quantity, transaction_date) 
+values ('Kopi', 'Konsumsi', 15, '2021-05-10'::timestamp);
+update public.product set available = 90 - 15, sold = 10 + 15 where id = 1;
 
-insert into public.transaction (product_id, quantity, created_at)
-values (3, 20, '2021-05-11'::timestamp);
-update public.product set available = 100 - 20 where id = 3;
+insert into public.transaction (product_name, category_name, quantity, transaction_date) 
+values ('Pasta Gigi', 'Pembersih', 20, '2021-05-11'::timestamp);
+update public.product set available = 100 - 20, sold = 20 where id = 3;
 
-insert into public.transaction (product_id, quantity, created_at)
-values (4, 30, '2021-05-11'::timestamp);
-update public.product set available = 100 - 30 where id = 4;
+insert into public.transaction (product_name, category_name, quantity, transaction_date) 
+values ('Sabun Mandi', 'Pembersih', 30, '2021-05-11'::timestamp);
+update public.product set available = 100 - 30, sold = 30 where id = 4;
 
-insert into public.transaction (product_id, quantity, created_at)
-values (5, 25, '2021-05-12'::timestamp);
-update public.product set available = 100 - 25 where id = 5;
+insert into public.transaction (product_name, category_name, quantity, transaction_date) 
+values ('Sampo', 'Pembersih', 25, '2021-05-12'::timestamp);
+update public.product set available = 100 - 25, sold = 25 where id = 5;
 
-insert into public.transaction (product_id, quantity, created_at)
-values (2, 5, '2021-05-12'::timestamp);
-update public.product set available = 81 - 5 where id = 2;
+insert into public.transaction (product_name, category_name, quantity, transaction_date) 
+values ('Teh', 'Konsumsi', 5, '2021-05-12'::timestamp);
+update public.product set available = 81 - 5, sold = 19 + 5 where id = 2;
